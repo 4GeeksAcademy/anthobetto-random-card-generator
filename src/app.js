7c09e81f37e5b071a2dae6dd8f1b560bd1f07068
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-08-11 09:20:43 +0000

    spain-77 Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let getRandomNumber = () => {
+    let cardsNumbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let getCardsNumbers = Math.floor(Math.random() * cardsNumbers.length);
+    return cardsNumbers[getCardsNumbers];
+  };
+  let getRandomSymbol = () => {
+    let cardSymbols = [
+      '<span style= "color: red;">♦</span>',
+      '<span style= "color: red;">♥</span>',
+      '<span style= "color: black;">♠</span>',
+      '<span style= "color: black;">♣</span>'
+    ];
+    let getCardsSymbols = Math.floor(Math.random() * cardSymbols.length);
+    return cardSymbols[getCardsSymbols];
+  };
+  let generateRandomCard = () => {
+    let randomSymbol = getRandomSymbol();
+    document.querySelector(".top-suit").innerHTML = randomSymbol;
+    document.querySelector(".bottom-suit").innerHTML = randomSymbol;
+    document.querySelector(".number-suit").innerHTML = getRandomNumber();
+  };
+  document.querySelector(".new-card").onclick = generateRandomCard;
 };
